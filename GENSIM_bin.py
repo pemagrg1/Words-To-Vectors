cccaa6d70437dbd3efa1addcb466b6040832c78b
Pema Gurung <30492527+pemagrg1@users.noreply.github.com>
2019-01-02 17:10:07 +0545

    Update GENSIM_bin.py

--- GENSIM_bin.py
+++ GENSIM_bin.py
@@ -3,7 +3,7 @@ import gensim
 from gensim.models import Word2Vec
 gensim_model = "/Users/pemagurung/Desktop/work/blogg/Use of gensim/word2vec-GoogleNews-vectors/GoogleNews-vectors-negative300.bin.gz"
 gensim_model = gensim.models.KeyedVectors.load_word2vec_format(gensim_model, binary=True, limit=100000)
-gensim_vector = model['dog']
+gensim_vector = gensim_model['dog']
 print(gensim_vector.shape)
 ##for 2D
 test = gensim_vector.reshape(1, -1)
